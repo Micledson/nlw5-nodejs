2ab2a6a3be77beabfdc421d6b433863e9a30c055
Micledson Carlos <micledson14@gmail.com>
2021-04-19 16:02:05 -0300

    criando umas rotas bem basicas

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.json({ message: "eae patrão" }));
+
+app.post("/", (req, res) => res.json({ message: "user salvo!" }));
+
+app.listen(3333, () => console.log("rodando na porta http://localhost:3333"));
