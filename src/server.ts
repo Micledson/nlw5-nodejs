4b3f0e9f816d34902f0dcda3e1ababb4f6df3160
Micledson Carlos <micledson14@gmail.com>
2021-04-20 12:05:01 -0300

    criando settingsRepository

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
-
-app.get("/", (req, res) => res.json({ message: "eae patrão" }));
-
-app.post("/", (req, res) => res.json({ message: "user salvo!" }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("rodando na porta http://localhost:3333"));
